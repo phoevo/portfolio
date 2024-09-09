1809ce0d417bcb97fd8fc3e91744732c906601c0
Phoevo <148871904+phoevo@users.noreply.github.com>
2024-09-09 21:27:50 +0300

    Made improvements to each page including: animation, positioning and content. About page is fresh and needs work.

--- portfolio.js
+++ portfolio.js
@@ -2,11 +2,26 @@ document.addEventListener("DOMContentLoaded", function(){
 
     let mid = document.querySelector(".mid");
     let workBtn = document.querySelector(".workBtn");
+    let aboutBtn = document.querySelector(".aboutBtn");
     workBtn.addEventListener("click", function(){
         mid.style.transform = "translateX(2000px)";
-        mid.style.transition = "500ms";
+        mid.style.transition = "700ms";
+        setTimeout(() => {
+            location.replace("portfolio-work.html")
+        }, 200);
+        
+    })
+
+    aboutBtn.addEventListener("click", function(){
+        mid.style.transform = "translateX(-2000px)";
+        mid.style.transition = "700ms";
+        setTimeout(() => {
+            location.replace("portfolio-about.html")
+        }, 200);
+
     })
 
 
-
 })
+
+
